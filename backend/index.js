4f6f207853242657d32aff87ae680fda4876d1ae
landr3s <laskindr3s@gmail.com>
2025-02-04 18:06:24 -0500

    Second commit

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT ?? 3000
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+  res.json('Hello world')
+})
+
 app.use('/reportes', reporteRoutes)
 
 app.get('/', (req, res) => {
